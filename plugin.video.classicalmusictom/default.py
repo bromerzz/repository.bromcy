932fa80b457e1976dc0857a4d4c156cf309e8993
bromerzz <bromerzz@me.com>
2017-08-11 17:40:37 +0300

    operav0.0.7
    #out line99 and 101

--- plugin.video.classicalmusictom/default.py
+++ plugin.video.classicalmusictom/default.py
@@ -96,12 +96,12 @@ YOUTUBE_CHANNEL_ID_6 = "PL1mZeR4wTBoyFZMFCUsPqpgGO2tPd4eAe"
 def Main_Menu():
 
 # If debug mode is enabled show the koding tutorials
-    if debug == 'true':
+    #if debug == 'true':
         Add_Dir ( '[COLOR=lime]Koding Tutorials[/COLOR]', '', "tutorials", True, '', '', '' )
-    else:
+    #else:
         Add_Dir ( '[COLOR=lime]Enable debug mode for some cool dev tools![/COLOR]', '', "koding_settings", False, '', '', '' )
     
-    my_message= "{'title' : 'Support & Suggestions', 'msg' : \"If you come across any online content you'd like to get added please use the support thread at noobsandnerds.com and I'll be happy to look into it for you.\"}"
+    #my_message= "{'title' : 'Support & Suggestions', 'msg' : \"If you come across any online content you'd like to get added please use the support thread at noobsandnerds.com and I'll be happy to look into it for you.\"}"
         # An example title/message we're going to send through to a popup dialog in the first Add_Dir item
 
 # Add some YT Playlists (see we're using BASE as the url)
