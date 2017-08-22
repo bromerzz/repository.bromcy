c41008a17f58c1edf48723eff791add0de172731
bromerzz <bromerzz@me.com>
2017-08-22 21:10:26 +0300

    Opera48
    #out line149 to 151

--- plugin.video.tomsoperacollection/default.py
+++ plugin.video.tomsoperacollection/default.py
@@ -145,10 +145,10 @@ def Main_Menu():
     Add_Dir(
         name="La Traviata",url=BASE3+YOUTUBE_CHANNEL_ID_7, folder=False, mode='play_yt',
         icon="/")
-	
-	Add_Dir(
-        name="Bizet, Carmen with Jonas Kaufman",url=BASE3+YOUTUBE_CHANNEL_ID_8, folder=False, mode='play_yt',
-        icon="/")
+
+	#Add_Dir(
+        #name="Bizet, Carmen with Jonas Kaufman",url=BASE3+YOUTUBE_CHANNEL_ID_8, folder=False, mode='play_yt',
+        #icon="/")
 
     Add_Dir(
         name="Placido Domingo, Selection",url=BASE+YOUTUBE_CHANNEL_ID_9, folder=False, mode='play_yt',
