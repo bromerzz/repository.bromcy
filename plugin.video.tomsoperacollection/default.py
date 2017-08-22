c6c68eb111edd2eb11780c4ed813c4a5eb64bb0d
bromerzz <bromerzz@me.com>
2017-08-22 20:48:29 +0300

    Opera46
    Corrected xml line error

--- plugin.video.tomsoperacollection/default.py
+++ plugin.video.tomsoperacollection/default.py
@@ -147,9 +147,9 @@ def Main_Menu():
         icon="/")
 	
 	Add_Dir(
-        name="Bize, Carmen with Jonas Kaufman",url=BASE3+YOUTUBE_CHANNEL_ID_8, folder=False, mode='play_yt',
+        name="Bizet, Carmen with Jonas Kaufman",url=BASE3+YOUTUBE_CHANNEL_ID_8, folder=False, mode='play_yt',
         icon="/")
-
+    
 	Add_Dir(
         name="Placido Domingo, Selection",url=BASE+YOUTUBE_CHANNEL_ID_9, folder=False, mode='play_yt',
         icon="/")
