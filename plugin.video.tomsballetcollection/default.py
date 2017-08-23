a50d53d6fc75c895094c5f5437343aab6c741104
bromerzz <bromerzz@me.com>
2017-08-23 22:17:56 +0300

    Ballet 26
    change add dir line content

--- plugin.video.tomsballetcollection/default.py
+++ plugin.video.tomsballetcollection/default.py
@@ -128,20 +128,16 @@ def Tchaikovsky_Menu():
 
 
     Add_Dir( 
-        name="Swan Lake, Kirov Ballet", url=BASE3+YOUTUBE_CHANNEL_ID_1+, folder=False,
-        mode='play_yt')
+        name="Swan Lake, Kirov Ballet", url=BASE3+YOUTUBE_CHANNEL_ID_1+, folder=False, mode='play_yt')
 
     Add_Dir( 
-        name="Nutcracker Suite", url=BASE3+YOUTUBE_CHANNEL_ID_2+, folder=False,
-        mode='play_yt')
+        name="Nutcracker Suite", url=BASE3+YOUTUBE_CHANNEL_ID_2+, folder=False, mode='play_yt')
 
     Add_Dir( 
-        name="Sleeping Beauty", url=BASE3+YOUTUBE_CHANNEL_ID_3+, folder=False,
-        mode='play_yt')
+        name="Sleeping Beauty", url=BASE3+YOUTUBE_CHANNEL_ID_3+, folder=False, mode='play_yt')
 
     Add_Dir( 
-        name="Iolanta", url=BASE3+YOUTUBE_CHANNEL_ID_5+, folder=False,
-        mode='play_yt')
+        name="Iolanta", url=BASE3+YOUTUBE_CHANNEL_ID_5+, folder=False, mode='play_yt')
 
 
 @route(mode='national de paris_menu') 
@@ -149,8 +145,7 @@ def National de Paris_Menu():
 
 
     Add_Dir( 
-        name="Cinderalla", url=BASE3+YOUTUBE_CHANNEL_ID_4+, folder=False,
-        mode='play_yt')
+        name="Cinderalla", url=BASE3+YOUTUBE_CHANNEL_ID_4+, folder=False, mode='play_yt')
 
 
 
@@ -158,24 +153,19 @@ def National de Paris_Menu():
 def London Childrens Ballet_Menu():  
 
     Add_Dir( 
-        name="A Little Princess", url=BASE3+YOUTUBE_CHANNEL_ID_6+, folder=False,
-        mode='play_yt')
+        name="A Little Princess", url=BASE3+YOUTUBE_CHANNEL_ID_6+, folder=False, mode='play_yt')
 
     Add_Dir( 
-        name="Rumpelstiltskin", url=BASE3+YOUTUBE_CHANNEL_ID_7+, folder=False,
-        mode='play_yt')
+        name="Rumpelstiltskin", url=BASE3+YOUTUBE_CHANNEL_ID_7+, folder=False, mode='play_yt')
 
     Add_Dir( 
-        name="The Secret Garden", url=BASE3+YOUTUBE_CHANNEL_ID_8+, folder=False,
-        mode='play_yt')
+        name="The Secret Garden", url=BASE3+YOUTUBE_CHANNEL_ID_8+, folder=False, mode='play_yt')
 
     Add_Dir( 
-        name="Snow White", url=BASE3+YOUTUBE_CHANNEL_ID_9+, folder=False,
-        mode='play_yt')
+        name="Snow White", url=BASE3+YOUTUBE_CHANNEL_ID_9+, folder=False, mode='play_yt')
 
     Add_Dir( 
-        name="The Canterville Ghost", url=BASE3+YOUTUBE_CHANNEL_ID_10+, folder=False,
-        mode='play_yt')
+        name="The Canterville Ghost", url=BASE3+YOUTUBE_CHANNEL_ID_10+, folder=False, mode='play_yt')
 
 
 
