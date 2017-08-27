6939e062bf689863b87268f5962575093d472e00
bromerzz <bromerzz@me.com>
2017-08-28 01:37:41 +0300

    Ballet 47
    swopped play a yt video code from fishing streams

--- plugin.video.tomsballetcollection/default.py
+++ plugin.video.tomsballetcollection/default.py
@@ -100,6 +100,10 @@ YOUTUBE_CHANNEL_ID_9 = "GD8IT91Xy90"
 # This is the main menu we open into
 @route(mode='main_menu')
 def Main_Menu():
+
+
+
+
     Add_Dir(name="Tchaikovsky", url=' ', mode="tchaikovsky_menu", folder=True,)
     Add_Dir(name="London Childrens Ballet", url=' ', mode="london childrens ballet_menu", folder=True,)
 
@@ -136,7 +140,8 @@ def Simple_Dialog(title,msg):
 # Play a youtube video
 @route(mode='play_yt', args=['url'])
 def Play_YT(url):
-    xbmc.Player().play('plugin://plugin.video.youtube/'+url)
+    xbmc.executebuiltin('PlayMedia(plugin://plugin.video.youtube/%s)'%url)       
+
 #----------------------------------------------------------------
 """
     SECTION 6:
