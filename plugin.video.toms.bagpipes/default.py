82762dbf62ddfa0e33768ffddd14f52ffc239527
bromerzz <bromerzz@me.com>
2017-09-16 09:48:50 +0300

    pipes19
    #edline 13

--- plugin.video.toms.bagpipes/default.py
+++ plugin.video.toms.bagpipes/default.py
@@ -77,7 +77,7 @@ YOUTUBE_CHANNEL_ID_9 = "6Y5mcLxlSgU"
 YOUTUBE_CHANNEL_ID_10 = "P2gDkcpYnMc&t"
 YOUTUBE_CHANNEL_ID_11 = "RDQut2getKFT4"
 #YOUTUBE_CHANNEL_ID_12 = "U89Qtbnk-Q4"
-YOUTUBE_CHANNEL_ID_13 = "amNF_F6oeRU"
+#YOUTUBE_CHANNEL_ID_13 = "amNF_F6oeRU"
 YOUTUBE_CHANNEL_ID_14 = "UAOepXDoLlg"
 YOUTUBE_CHANNEL_ID_15 = "RsxjXArvdXU"
 YOUTUBE_CHANNEL_ID_16 = "Qut2getKFT4"
@@ -164,7 +164,7 @@ def Other_Military_Tattoos_Menu():
 @route(mode='general_piping_content_menu')
 def General_Piping_Content_Menu():
     Add_Dir(name="Cadets Pipe Band", url=BASE3+YOUTUBE_CHANNEL_ID_17,folder=False, mode='play_yt',)
-	Add_Dir(name="Duelling Pipers", url=BASE3+YOUTUBE_CHANNEL_ID_13, folder=False, mode='play_yt',)
+	#Add_Dir(name="Duelling Pipers", url=BASE3+YOUTUBE_CHANNEL_ID_13, folder=False, mode='play_yt',)
 	Add_Dir(name="Andre Rieu Amazing Grace", url=BASE3+YOUTUBE_CHANNEL_ID_14, folder=False, mode='play_yt',)
 	Add_Dir(name="Mixed Selection", url=BASE3+YOUTUBE_CHANNEL_ID_15, folder=False, mode='play_yt',)
 	Add_Dir(name="Mixed Selection 2", url=BASE3+YOUTUBE_CHANNEL_ID_16, folder=False, mode='play_yt',)
