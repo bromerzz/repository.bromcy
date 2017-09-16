be866c96c5ea373f2d836f6b5128255da55ec244
bromerzz <bromerzz@me.com>
2017-09-16 10:08:14 +0300

    pipes23
    16 and 18 ####

--- plugin.video.toms.bagpipes/default.py
+++ plugin.video.toms.bagpipes/default.py
@@ -80,9 +80,9 @@ YOUTUBE_CHANNEL_ID_11 = "RDQut2getKFT4"
 #YOUTUBE_CHANNEL_ID_13 = "amNF_F6oeRU"
 #YOUTUBE_CHANNEL_ID_14 = "UAOepXDoLlg"
 YOUTUBE_CHANNEL_ID_15 = "RsxjXArvdXU"
-YOUTUBE_CHANNEL_ID_16 = "Qut2getKFT4"
+#YOUTUBE_CHANNEL_ID_16 = "Qut2getKFT4"
 #YOUTUBE_CHANNEL_ID_17 = "x-xiiK5Y2BU"
-YOUTUBE_CHANNEL_ID_18 = "b8ZBoId14Q"
+#YOUTUBE_CHANNEL_ID_18 = "b8ZBoId14Q"
 #YOUTUBE_CHANNEL_ID_19 = "dMzQiu2mUlE"
 #YOUTUBE_CHANNEL_ID_20 = "5xtGjHT0iVc"
 YOUTUBE_CHANNEL_ID_21 = "cjDXWPW9f9s"
@@ -167,8 +167,8 @@ def General_Piping_Content_Menu():
 	#Add_Dir(name="Duelling Pipers", url=BASE3+YOUTUBE_CHANNEL_ID_13, folder=False, mode='play_yt',)
 	#Add_Dir(name="Andre Rieu Amazing Grace", url=BASE3+YOUTUBE_CHANNEL_ID_14, folder=False, mode='play_yt',)
 	Add_Dir(name="Mixed Selection", url=BASE3+YOUTUBE_CHANNEL_ID_15, folder=False, mode='play_yt',)
-	Add_Dir(name="Mixed Selection 2", url=BASE3+YOUTUBE_CHANNEL_ID_16, folder=False, mode='play_yt',)
-    Add_Dir(name="Celtic Tunes", url=BASE3+YOUTUBE_CHANNEL_ID_18, folder=False, mode='play_yt',)
+	#Add_Dir(name="Mixed Selection 2", url=BASE3+YOUTUBE_CHANNEL_ID_16, folder=False, mode='play_yt',)
+    #Add_Dir(name="Celtic Tunes", url=BASE3+YOUTUBE_CHANNEL_ID_18, folder=False, mode='play_yt',)
     #Add_Dir(name="Celtic Bagpipes  Stephen Megarity", url=BASE3+YOUTUBE_CHANNEL_ID_19, folder=False, mode='play_yt',)
     #Add_Dir(name="RCMP Menin Gate", url=BASE3+YOUTUBE_CHANNEL_ID_20, folder=False, mode='play_yt',)
     #Add_Dir(name="", url=BASE3+YOUTUBE_CHANNEL_ID_+"/", folder=False, mode='play_yt',)
