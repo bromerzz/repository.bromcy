8b95c872362384c2e0813e7d82b7e883430fb0de
bromerzz <bromerzz@me.com>
2017-09-16 08:59:32 +0300

    pipes14
    #ed out other section

--- plugin.video.toms.bagpipes/default.py
+++ plugin.video.toms.bagpipes/default.py
@@ -163,14 +163,14 @@ def Other_Military_Tattoos_Menu():
 
 @route(mode='other_piping_content_menu')
 def Other_Piping_Content_Menu():
-    Add_Dir(name="Cadets Pipe Band", url=BASE3+YOUTUBE_CHANNEL_ID_17,folder=False, mode='play_yt')
-	Add_Dir(name="Duelling Pipers", url=BASE3+YOUTUBE_CHANNEL_ID_13, folder=False, mode='play_yt',)
-	Add_Dir(name="Andre Rieu Amazing Grace", url=BASE3+YOUTUBE_CHANNEL_ID_14, folder=False, mode='play_yt',)
-	Add_Dir(name="Various", url=BASE3+YOUTUBE_CHANNEL_ID_15, folder=False, mode='play_yt',)
-	Add_Dir(name="Various 2", url=BASE3+YOUTUBE_CHANNEL_ID_16, folder=False, mode='play_yt',)
-    Add_Dir(name="Celtic Tunes", url=BASE3+YOUTUBE_CHANNEL_ID_18, folder=False, mode='play_yt',)
-    Add_Dir(name="Celtic Bagpipes  Stephen Megarity", url=BASE3+YOUTUBE_CHANNEL_ID_19, folder=False, mode='play_yt',)
-    Add_Dir(name="RCMP Menin Gate", url=BASE3+YOUTUBE_CHANNEL_ID_20, folder=False, mode='play_yt',)
+    #Add_Dir(name="Cadets Pipe Band", url=BASE3+YOUTUBE_CHANNEL_ID_17,folder=False, mode='play_yt',)
+	#Add_Dir(name="Duelling Pipers", url=BASE3+YOUTUBE_CHANNEL_ID_13, folder=False, mode='play_yt',)
+	#Add_Dir(name="Andre Rieu Amazing Grace", url=BASE3+YOUTUBE_CHANNEL_ID_14, folder=False, mode='play_yt',)
+	#Add_Dir(name="Various", url=BASE3+YOUTUBE_CHANNEL_ID_15, folder=False, mode='play_yt',)
+	#Add_Dir(name="Various 2", url=BASE3+YOUTUBE_CHANNEL_ID_16, folder=False, mode='play_yt',)
+    #Add_Dir(name="Celtic Tunes", url=BASE3+YOUTUBE_CHANNEL_ID_18, folder=False, mode='play_yt',)
+    #Add_Dir(name="Celtic Bagpipes  Stephen Megarity", url=BASE3+YOUTUBE_CHANNEL_ID_19, folder=False, mode='play_yt',)
+    #Add_Dir(name="RCMP Menin Gate", url=BASE3+YOUTUBE_CHANNEL_ID_20, folder=False, mode='play_yt',)
     #Add_Dir(name="", url=BASE3+YOUTUBE_CHANNEL_ID_+"/", folder=False, mode='play_yt',)
 
 #----------------------------------------------------------------
